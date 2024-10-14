34390f37781743541a3c1297c6e33bfa4ca75c93
Barnabé Frimaudeau <frimaudeau.barnabe@gmail.com>
2024-10-14 19:17:25 -0400

    Initial setup

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Board from './components/Board'
+
+function App() {
+  return (
+    <header>
+      <h1>Tic-tac-toe!</h1>
+      <Board />
+    </header>
+  )
+}
+
+export default App
